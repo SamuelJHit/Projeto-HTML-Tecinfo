c86b739a2f654d284ae635d07e01e0bbd5883ee1
Samuel Justino <samueljustinotecnifo21@gmail.com>
2026-05-18 21:04:46 -0300

    Adicionando java para interação de login

---
+++ PainelSistemaGeren/index.js
@@ -0,0 +1,11 @@
+function entrar() {
+    let login = document.getElementById("login");
+    let senha = document.getElementById("senha");
+    
+    if (login.value == "" || senha.value == "") {
+        alert ("Preencha usuario e senha!!")
+    } else {
+        alert ("Login realizado!")
+        window.location.href = "pagina.html";
+    }
+}
