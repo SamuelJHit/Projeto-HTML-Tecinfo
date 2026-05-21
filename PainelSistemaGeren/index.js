9e4be2814247ed8e3f3a76c47f063b06c26f1fa0
Samuel Justino <samuelragnarson9@gmail.comgit config --global user.email samuelragnarson9gmail.comgit config --global user.name Samuel>
2026-05-21 12:33:42 -0300

    Adicionando mais função

--- PainelSistemaGeren/index.js
+++ PainelSistemaGeren/index.js
@@ -5,9 +5,23 @@ function entrar() {
     if (login.value == "" || senha.value == "") {
         alert ("Preencha usuario e senha!!")
     } else {
+        localStorage.setItem("login", login.value);
+
         alert ("Login realizado!")
         window.location.href = "pagina.html";
     }
 
-    localStorage.setItem("login", login);
+}
+
+function dashboard() {
+    let login = document.getElementById("login");
+
+    if (login == ""){
+        console.log("Desconhecido entrando")
+    } else {
+        localStorage.setItem("login", login);
+
+        console.log("Usuario entrando")
+        window.location.href = "dashboard.html";
+    }
 }
